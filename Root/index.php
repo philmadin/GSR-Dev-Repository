1389925a2b805e015abe9ed3c1ea6cbf0197ee34
Phil Madin <philip.madin@students.mq.edu.au>
2017-03-11 00:39:17 +1100

    Removed coffee

--- Root/index.php
+++ Root/index.php
@@ -60,89 +60,13 @@
         	<div id="pagination"></div>
     	</div>
 
-				<!--<div id="featured_articles_container">
-					<ul id="featured_articles">
-                        <?php
-                        $featured_ar = mysqli_query($con, "SELECT * FROM tbl_featured WHERE position!=0 AND article_type!='Video' ORDER BY position ASC LIMIT 5");
-
-                        while ($ft_row = mysqli_fetch_assoc($featured_ar)) {
-                            $article_type = $ft_row['article_type'];
-                            $article_id = $ft_row['article_id'];
-                            $video_id = $ft_row['video_id'];
-                            if($article_type=="Video"){
-                                $ft_title = $v[$video_id]['snippet']['title'];
-                                $ft_image = "http://img.youtube.com/vi/".$video_id."/maxresdefault.jpg";
-                                $ft_url   = "video.php?id=".$video_id;
-                                $ft_summary = null;
-                            }
-                            if($article_type=="Guide"){
-                                $guide_ar = mysqli_query($con, "SELECT * FROM tbl_guide WHERE id = $article_id");
-                                while ($guide_row = mysqli_fetch_assoc($guide_ar)) {
-                                    $ft_title = $guide_row['title'];
-                                    $ft_images 	= unserialize($guide_row['images']);
-                                    $ft_image   = "imgs/guide/".urlencode($ft_images[0]);
-                                    $ft_url     = "guide.php?t=" . urlencode(str_replace(" ", "_", $ft_title));
-                                    $ft_summary = null;
-                                }
-                            }
-                            if($article_type=="Opinion"){
-                                $opinion_ar = mysqli_query($con, "SELECT * FROM tbl_opinion WHERE id = $article_id");
-                                while ($op_row = mysqli_fetch_assoc($opinion_ar)) {
-                                    $ft_title = $op_row['title'];
-                                    $ft_image = "imgs/opinion/".urlencode($op_row['a_image']);
-                                    $ft_url     = "opinion.php?t=" . urlencode(str_replace(" ", "_", $ft_title));
-                                    $ft_summary = null;
-                                }
-                            }
-                            if($article_type=="News"){
-                                $news_ar = mysqli_query($con, "SELECT * FROM tbl_news WHERE id = $article_id");
-                                while ($op_row = mysqli_fetch_assoc($news_ar)) {
-                                    $ft_title = $op_row['title'];
-                                    $ft_image = "imgs/news/".urlencode($op_row['a_image']);
-                                    $ft_url     = "news.php?t=" . urlencode(str_replace(" ", "_", $ft_title));
-                                    $ft_summary = null;
-                                }
-                            }
-                            if($article_type=="Review"){
-                                $review_ar = mysqli_query($con, "SELECT * FROM tbl_review WHERE id = $article_id");
-                                while ($rvw_row = mysqli_fetch_assoc($review_ar)) {
-                                    $ft_title = $rvw_row['title'];
-                                    $ft_game	= $rvw_row['gamename'];
-                                    $ft_image = "imgs/review/".urlencode($rvw_row['a_image']);
-                                    $ft_url	= "review.php?t=" . urlencode(str_replace(" ", "_", $ft_title)) . "&g=" . urlencode(str_replace(" ", "_", $ft_game));
-                                    $ft_summary = $rvw_row['summary'];
-                                }
-                            }
-
-
-                        ?>
-			<li class="featured_item">
-				<a href="<?php echo $ft_url; ?>" id="featured_content" style="background-image:url(<?php echo $ft_image; ?>);">
-					<h3 id="featured_title"><?php echo $ft_title; ?></h3>
-                                    	<?php if(isset($ft_summary)){ ?>
-						<p id="featured_summary"><?php echo $ft_summary; ?></p>
-					<?php } ?>
-                                    	<span id="type"><?php echo $article_type;?></span>
-                                    	<span id="overlay"></span>
-				</a>
-			</li>
-                        <?php } ?>
-                     </ul>
-		</div>-->
+			
 
 
 	<div id="page" class="container_24">
 
 		<article class="blog">
 			<section class="grid_18">
-				<h1>EXCLUSIVE</h1>
-				<div id="exclusive" class="owl-carousel owl-theme">
-						<div class="item"><a href="https://www.gofundme.com/tecjc6y4" class="exclusive_item whitegold">
-							<img src="imgs/gsr_exclusives_donate.jpg">
-							<span class="primary">Love GSR?</span><br>
-							<span class="secondary">Buy us a coffee!</span>
-						</a></div>
-				</div>
 
 				<h2>Latest Articles</h2>
 
