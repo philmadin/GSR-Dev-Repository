a81c146d09de11c78872f35ce146ed0fe331a593
philmadin <philip.madin@students.mq.edu.au>
2017-05-27 23:49:09 +1000

    Update asideFetch.php

--- Root/asideFetch.php
+++ Root/asideFetch.php
@@ -200,7 +200,14 @@ AND timestamp < curdate() - INTERVAL DAYOFWEEK(curdate())-9 DAY AND article_type
 	</dl>
 	<div id="message">
 		Contribute<br><br>
-		Support your favourite game reviewers by <a href="https://www.gofundme.com/tecjc6y4">contributing here</a>.
+		Support your favourite game reviewers by
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="YANHDL9KQMFJL">
+			<input type="image" src="https://www.paypalobjects.com/en_AU/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_AU/i/scr/pixel.gif" width="1" height="1">
+		</form>
+ <a href="https://www.gofundme.com/tecjc6y4">contributing here</a>.
 	</div>
 <ins data-revive-zoneid="1" data-revive-id="92efd1a11555b4b462d64394af1b51db"></ins>
 <script async src="//gamesharkreviews.com/adserver/www/delivery/asyncjs.php"></script>
