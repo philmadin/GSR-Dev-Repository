99cada693e68f6b0105186578ab6e626c858f70d
Phil Madin <philip.madin@students.mq.edu.au>
2017-06-28 13:34:05 +1000

    Changes to youtube API JSON file generated from data

--- Root/videos-get.php
+++ Root/videos-get.php
@@ -1,75 +1,93 @@
 <?php
 
+header('Content-Type: application/json');
 include 'mysql_con.php';
-
+set_time_limit(10000);
 
 function channelVideos(){
     global $con;
-$baseUrl = 'https://www.googleapis.com/youtube/v3/';
-$apiKey = 'AIzaSyDx5v2Wl6vpDwysvaafFaPsK4WaXR_mV6Q';
-$channelId = 'UCTfKW979nHGW5KW0WtV9Nbw';
-$videos = array();
-$url = $baseUrl .'channels?' .
+	$baseUrl = 'https://www.googleapis.com/youtube/v3/';
+	$apiKey = 'AIzaSyDx5v2Wl6vpDwysvaafFaPsK4WaXR_mV6Q';
+	$channelId = 'UCOP7Xx-EBE6vaV_ThC-h3cw';
+	$videos = array();
+	$url = $baseUrl .'channels?' .
     'id=' . $channelId .
     '&part=contentDetails,statistics' .
     '&key=' . $apiKey;
-$json = json_decode(file_get_contents($url), true);
-$videos['channel'] = $json['items'][0];
-$playlist = $json['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+	$json = json_decode(file_get_contents($url), true);
+	$locals['channel'] = $json['items'][0];
+	$playlist = $json['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
  
-$url = $baseUrl .'playlistItems?' .
- 'part=snippet' .
- '&maxResults=5' .
- '&playlistId=' . $playlist .
- '&key=' . $apiKey;
-$json = json_decode(file_get_contents($url), true);
- 
-foreach($json['items'] as $video)
-    $videos['i'][] = $video['snippet']['resourceId']['videoId'];
- 
-while(isset($json['nextPageToken'])){
-    $nextUrl = $url . '&pageToken=' . $json['nextPageToken'];
-    $json = json_decode(file_get_contents($nextUrl), true);
-    foreach($json['items'] as $video)
-        $videos['i'][] = $video['snippet']['resourceId']['videoId'];
-}
+	$url = $baseUrl .'playlistItems?' .
+ 	'part=snippet,contentDetails' .
+ 	'&maxResults=5' .
+ 	'&playlistId=' . $playlist .
+ 	'&key=' . $apiKey;
+	$json = json_decode(file_get_contents($url), true);
+	$z=0;
+	foreach($json['items'] as $video){
+		array_push($videos,$z);
+		$videos[$z]=array();
+		$videos[$z]['id']=$video['snippet']['resourceId']['videoId'];
+    	$z++;
+	}
+	while(isset($json['nextPageToken'])){
+		$nextUrl = $url . '&pageToken=' . $json['nextPageToken'];
+		$json = json_decode(file_get_contents($nextUrl), true);
+		foreach($json['items'] as $video){
+			array_push($videos,$z);
+			$videos[$z]=array();
+			$videos[$z]["id"]=$video['snippet']['resourceId']['videoId'];
+			$z++;
+		}
+	}
 
-for ($x = 0; $x <= (count($videos['i'])-1); $x++) {
-$url2 = $baseUrl .'videos?' .
-	'id=' . $videos['i'][$x] .
-	'&part=contentDetails,snippet,statistics' . 
-	'&key=' . $apiKey;
-	$json2 = json_decode(file_get_contents($url2), true);
-    $videos['i'][$x] = $json2['items'][0];
-}
 
-foreach($videos['i'] as $vID){
-	$videos[$vID['id']] = $vID;
-    $textdesc = $vID['snippet']['description'];
-    preg_match_all('/{(.*?)}/', $textdesc, $matches);
-    $user_array = $matches[1];
-    $user_ar = explode(",",$user_array[0]);
-    foreach($user_ar as $user){
-        $query = mysqli_query($con, "SELECT * FROM tbl_accounts WHERE username='$user'");
-        while($row = mysqli_fetch_assoc($query)) {
-            if ($row['videos']!="") {
-                $vidlist = explode(",", $row['videos']);
-                if(!in_array($vID['id'], $vidlist)){
-                    array_push($vidlist,$vID['id']);
-                    $sql = "UPDATE tbl_accounts SET videos='".implode(",", $vidlist)."' WHERE username='$user'";
-                    if (mysqli_query($con, $sql)) {}
-                }
-            }
-            else{
-                $sql = "UPDATE tbl_accounts SET videos='".$vID['id']."' WHERE username='$user'";
+	for ($x = 0; $x < (count($videos)-1); $x++) {
+		$url2 = $baseUrl .'videos?' .
+		'id=' . $videos[$x]['id'] .
+		'&part=snippet,contentDetails,statistics' . 
+		'&key=' . $apiKey;
+		$json2 = json_decode(file_get_contents($url2), true);
+		$videos[$x]['itemDetails'] = $json2['items'][0];
+	}
 
-                if (mysqli_query($con, $sql)) {}
-            }
-        }
-    }
-} 
+/*
+	//echo '<pre>';
+	//print_r($videos);
+	//echo '</pre>';
 
-return $videos;
+	foreach($videos as $vID){
+		print_r($vID);
+		$videos[$vID['id']] = $vID;
+		$textdesc = $vID['snippet']['description'];
+		preg_match_all('/{(.*?)}/', $textdesc, $matches);
+		$user_array = $matches[1];
+		$user_ar = explode(",",$user_array[0]);
+		foreach($user_ar as $user){
+			$query = mysqli_query($con, "SELECT * FROM tbl_accounts WHERE username='$user'");
+			while($row = mysqli_fetch_assoc($query)) {
+				if ($row['videos']!="") {
+					$vidlist = explode(",", $row['videos']);
+					if(!in_array($vID['id'], $vidlist)){
+						array_push($vidlist,$vID['id']);
+						$sql = "UPDATE tbl_accounts SET videos='".implode(",", $vidlist)."' WHERE username='$user'";
+						if (mysqli_query($con, $sql)) {}
+					}
+				}
+				else{
+					$sql = "UPDATE tbl_accounts SET videos='".$vID['id']."' WHERE username='$user'";
+
+					if (mysqli_query($con, $sql)) {}
+				}
+			}
+		}
+	} */
+	//echo '<pre>';
+	//print_r($videos);
+	//echo '</pre>';
+	
+	return json_encode(array($videos));
 }
 
 $v = channelVideos();
@@ -104,7 +122,7 @@ function totalViews()
 }
 
 if(isset($_GET['video-list'])){
-echo json_encode(channelVideos());
+echo channelVideos();
 }
 
 if(isset($_GET['suggested-videos'])){
