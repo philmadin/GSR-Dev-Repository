bfdd4afa1846e04ba641180f57a08d16b7ee42c5
michellehusbands <mrhc93@gmail.com>
2017-08-14 15:38:59 +1000

    Fixed bugs with the prepared statements and put the photo gallery

--- Root/mysql_con.php
+++ Root/mysql_con.php
@@ -27,9 +27,10 @@ include_once("tokengen.php");
 /* Prepared Statements */
 $query_user_info = "SELECT * FROM tbl_accounts WHERE username = ?";
 $query_user_exp = "SELECT * FROM tbl_xp_log WHERE username = ? AND action_type = ?";
-$query_user_frndachi = "SELECT username FROM tbl_accounts WHERE id = ?";
-//$query_user_achieve = "SELECT * FROM tbl_accounts WHERE id IN (?)";
+$query_user_friend = "SELECT * FROM tbl_accounts WHERE id = ?";
+$query_user_badges = "SELECT * FROM tbl_badges WHERE id = ?";
 $query_user_articles = "SELECT id FROM tbl_guide WHERE authuser = ? UNION SELECT id FROM tbl_news WHERE authuser = ? UNION SELECT id FROM tbl_review WHERE authuser = ? UNION SELECT id FROM tbl_opinion WHERE authuser = ?";
 $query_add_friend = "SELECT * FROM tbl_requests WHERE requestee = ? AND requester = ?";
+$query_request_friend = "INSERT INTO tbl_requests (requester, requestee) VALUES (?, ?)";
 
 ?>
